1c925b2f4451e04f2ee21c6e2dad6b56d97421be
Dani <danillano97@gmail.com>
2021-10-16 23:46:09 -0300

    tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -35,7 +35,7 @@ contract TodoList {
 
     function toggleCompleted(uint _id) public {
         Task memory _task = tasks[_id];
-        _task.completed = !_task[_id];
+        _task.completed = !_task.completed;
         tasks[_id] = _task;
         emit TaskCompleted(_id, _task.completed);
     }
---
+++ src/app.js
@@ -0,0 +1,104 @@
+App = {
+    loading: false,
+    contracts: {},
+
+
+    load: async () => {
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        await App.render()
+    },
+
+    loadWeb3: async () => {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        } else {
+            window.alert("please start MetaMask")
+        }
+        // Modern dapp browsers
+        if (window.ethereum) {
+            window.web3 !== new Web3(ethereum)
+            try {
+                await ethereum.enable()
+                web3.eth.sendTransaction({/* ... */})
+            } catch (error) {
+                // error
+            }
+        }
+        // Legacy web3.0 browsers
+        else if (window.web3) {
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+            // Accounts always exposed
+            web3.eth.sendTransaction({/* ... */})
+        }
+        // Non dapp browsers
+        else {
+            console.log("Non ethereum browser detected. Please install MetaMask")
+        }
+    },
+
+    loadAccount: async () => {
+        App.count = web3.eth.accounts[0]
+    },
+    
+    loadContract: async () => {
+        const todoList = await $.getJSON('TodoList.json')
+        App.contracts.TodoList = TruffleContract(todoList)
+        App.contracts.todoList.setProvider(App.web3Provider)
+
+        App.todoList = await App.contracts.TodoList.deployed()
+    },
+
+    render: async () => {
+        if (App.loading) {
+            return
+        }
+
+        App.setLoading(true)
+
+        $('#account').html(App.Account)
+
+        await App.renderTasks()
+
+        App.setLoading(false)
+    },
+
+    renderTasks: async () => {
+        // Load the total taask count from the blockchain
+        const taskCount = await App.todoList.taskCount()
+        const $taskComplete = $('.taskTemplate')
+
+        for (var i = 1; i <= taskCount; i++) {
+            const task = await App.todoList.tasks(i)
+            const taskId = task[0].toNumber()
+            const taskContent = task[1]
+            const taaskCompleted = task[2]
+
+            const $newTaskTemplate = $taskTemplate.clone()
+            $newTaskTemplate.find('.content').html(taskContent)
+            $newTaskTemplate.find('input')
+                            .prop('name', taskId)
+                            .prop('checked', taskCompleted)
+                            .on('click', App.toggleCompleted)
+
+            if (taskCompleted) {
+                $('#completedTaskList').append($newTaskTemplate)
+            } else {
+                $('#taskList').append($newTaskTemplate)
+            }
+
+            // show the task
+            $newTaskTemplate.show()
+        }
+    },
+
+    createTask: async () => {
+        App.setLoading(true)
+        const content = $('#newTask').val()
+        await App.todoList.createTask(content)
+        window.location.reload()
+    }
+}
